21745f7d9f3c13ee6071f8df94101d5a25b7cd0d
FelipeB <andresfelipeba@ufps.edu.co>
2024-11-08 10:40:13 -0500

    Crear todos los repositorios para las tablas

---
+++ src/main/java/co/edu/ufps/repositories/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package co.edu.ufps.repositories;
+
+import co.edu.ufps.entities.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+}
